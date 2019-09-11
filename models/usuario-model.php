ef10420b79f4344c34392098fac065d1246af3c6
marcelodemoraes <marcelodemoraes11@gmail.com>
2019-09-11 01:11:06 -0300

    added xss sanitization

--- models/usuario-model.php
+++ models/usuario-model.php
@@ -4,10 +4,13 @@
 
 	class UsuarioModel extends Model {
 
-		public function cadastrarUsuario($dados){
+		public function cadastrarUsuario($login, $senha, $name){
+			$utils = $this->carregarModel("utils");
+			$arrayLimpo = $utils->xssFilter(array($login, $senha, $nome));
 			// 1 - Limpar e Validar os dados
 			// 2 - Queries para inserir no banco
 			// 3 - Precisa retornar algum dado? qual?
+			return $resultado;
 		}
 
 		public function buscarUsuario($dados){
--- models/utils-model.php
+++ models/utils-model.php
@@ -6,4 +6,19 @@
 
 		// @TODO Funções de uso compartilhado.
 
+		//Função que sanitiza input de usuário contra XSS
+		//Recebe um array de parametros e os encoda para html entitites
+		//Retorna um array, com parametros sanitizados na mesma ordem em que foi recebido 
+		public function xssFilter($params){
+			//Verifica se o input é um array
+			if(!(is_array($params)){
+				//@TODO mensagem de erro
+			}
+			//itera sobre o array e modifica seus valores
+			foreach($params as $value){
+				//função que encoda as variaveis
+				$value = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+			}
+			return $params;
+		}
 	}
