80225533455e07d3715aa90608325d7392e4f76c
alexandregjr <alexandregalochajr@gmail.com>
2019-09-12 14:54:32 -0300

    sessionModel criado

--- models/session-model.php
+++ models/session-model.php
@@ -6,14 +6,34 @@
 
 		public function criarSessao($dados){
 			// Cria uma nova sessao de login para o usuario atual
+			session_start();
+
+			if (!is_array($dados)) return false;
+			
+			$_SESSION['id'] = $dados['id'];
+			$_SESSION['role'] = $dados['role'];
+
+			return true;
 		}
 
 		public function destruirSessao($dados){
-			// Cria uma nova sessao de login para o usuario atual
+			// Deleta a sessao
+			$_SESSION = array();
+
+			if (ini_get("session.use_cookies")) {
+				$params = session_get_cookie_params();
+				setcookie(session_name(), '', time() - 42000,
+					$params["path"], $params["domain"],
+					$params["secure"], $params["httponly"]
+				);
+			}
+
+			session_destroy();
 		}
 
 		public function verificarSessao($dados){
 			// Verifica se o usuario possui uma sessao em aberto.
+			return isset($_SESSION['id']) ? true : false; 
 		}
 
 	}
